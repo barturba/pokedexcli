078585a7119ec83327801599fdefcbb68d0fc5e3
barturba <b@bartas.co>
2024-08-02 14:26:00 -0600

    feat: Update main.go to handle invalid commands gracefully

--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
+type cliCommand struct {
+	name string
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+		},
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
+	commands := getCommands()
+	for {
+		scanner.Scan()
+		err := scanner.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Println("pokedex > ")
-		fmt.Println(scanner.Text())
+		fmt.Println(commands[scanner.Text()].name)
 	}
 }
