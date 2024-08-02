a43297bc1bd483d7e7f4e2495ed720e5b06e4c1b
barturba <b@bartas.co>
2024-08-02 15:23:52 -0600

    Refactor main.go to improve command handling and user experience

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 type cliCommand struct {
 	name        string
 	description string
+	callback    func() error
 }
 
 func getCommands() map[string]cliCommand {
@@ -17,6 +18,12 @@ func getCommands() map[string]cliCommand {
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
 		},
 	}
 }
@@ -24,7 +31,7 @@ func getCommands() map[string]cliCommand {
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	commands := getCommands()
-	fmt.Printf("pokedex > ")
+	fmt.Printf("Pokedex > ")
 	for {
 		scanner.Scan()
 		err := scanner.Err()
@@ -32,10 +39,28 @@ func main() {
 			log.Fatal(err)
 		}
 		_, ok := commands[scanner.Text()]
+		fmt.Println("")
 		if ok {
-			fmt.Println(commands[scanner.Text()].name)
-			fmt.Println(commands[scanner.Text()].description)
+			err = commands[scanner.Text()].callback()
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		fmt.Printf("pokedex > ")
+		fmt.Printf("Pokedex > ")
 	}
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	fmt.Println("")
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
