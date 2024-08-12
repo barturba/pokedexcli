e8013493fe213bdc7cb4e12fe93b0184be31a406
barturba <b@bartas.co>
2024-08-12 13:29:11 -0600

    refactor: Add caching for explore command to improve performance

--- command_explore.go
+++ command_explore.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"github.com/barturba/pokedexcli/internal/pokeapi"
 )
 
 func commandExplore(cfg *config, areaName string) error {
+
 	fmt.Printf("Exploring %s...\n", areaName)
 
+	locationResp := pokeapi.RespShallowLocation{}
 	var err error
-	locationResp, err := cfg.pokeapiClient.ListLocation(&areaName)
-	if err != nil {
-		return err
+
+	val, ok := cfg.pokeCache.Get(areaName)
+	if ok {
+		err = json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("got explore data from cache")
+	} else {
+
+		locationResp, err = cfg.pokeapiClient.ListLocation(&areaName)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("got explore data from api")
+		dat, err := json.Marshal(&locationResp)
+		if err != nil {
+			return err
+		}
+		cfg.pokeCache.Add(areaName, dat)
 	}
+
 	fmt.Println("Found Pokemon:")
 	for _, pokemon := range locationResp.PokemonEncounters {
 		fmt.Printf(" - %s\n", pokemon.Pokemon.Name)
 	}
 	return nil
+
 }
